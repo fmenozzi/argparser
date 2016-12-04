31c2636a7f22bb197ad5fd6060f77b68906ad470
Federico Menozzi <federicogmenozzi@gmail.com>
2016-12-04 15:58:04 -0500

    Modify test.cpp

--- test.cpp
+++ test.cpp
@@ -1,43 +1,64 @@
 #include <iostream>
-#include <type_traits>
+#include <memory>
+#include <vector>
+#include <map>
 
-namespace ap {
-    enum class mode {
-        REQUIRED,
-        OPTIONAL
+#include "argparser.hpp"
+
+// Conversion specializations
+template<typename T> struct convert_from_string_to {};
+template<> struct convert_from_string_to<int> {
+    static int convert(const std::string& s) { return std::stoi(s); }
+};
+template<> struct convert_from_string_to<bool> {
+    static bool convert(const std::string& s) { return s == "1" || s == "true"; }
+};
+
+// Store functions to call for each arg string
+//std::map<std::string, std::function<void(void)>> m;
+std::vector<std::function<void(void)>> m;
+
+// Assign functions to call for each arg
+template<typename T>
+void add(const std::string& argstr, T* ptr, const std::string& s) {
+    /*
+    m[argstr] = [ptr, s] () {
+        *ptr = convert_from_string_to<T>::convert(s);
     };
-
-    class parser {
-    public:
-        parser(int argc, char* argv[]) {
-
-        }
-
-        template<typename T>
-        void add(const std::string& shortarg,
-                 const std::string& longarg,
-                 T* ptr,
-                 const std::string& helpstr,
-                 mode m = mode::REQUIRED) {
-
-            static_assert(std::is_default_constructible<T>::value, "Cannot default construct T");
-            *ptr = T{};
-            std::cout << "Parsemode " << (m == mode::REQUIRED ?  "REQUIRED" : "OPTIONAL") << "\n";
-
-        }
-    };
+    */
+    m.emplace_back([ptr, s]() {
+        *ptr = convert_from_string_to<T>::convert(s);
+    });
 }
 
+// Call each function
+void parse() {
+    /*
+    for (const auto& kv : m) {
+        kv.second();
+    }
+    */
+    for (const auto& f : m) {
+        f();
+    }
+}
 
 int main(int argc, char* argv[]) {
-    bool v;
-    int i;
+    int i = 1;
+    bool b = false;
 
-    ap::parser parser(argc, argv);
-    parser.add("-v", "--verbose", &v, "Verbose output");
-    parser.add("-i", "--integer", &i, "Integer argument", ap::mode::OPTIONAL);
+    add("-i", &i, "14");
+    add("-b", &b, "true");
 
-    std::cout << v << ", " << i << "\n";
+    std::cout << "i: " << i << "\n"
+              << "b: " << b << "\n";
+
+    parse();
+
+    std::cout << "i: " << i << "\n"
+              << "b: " << b << "\n";
+
+    ap::parser(argc, argv);
 
     return 0;
 }
